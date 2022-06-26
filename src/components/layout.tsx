6d734489b9269198c512c170b1a18a34791a88d7
Vojta Holik <vojta@egghead.io>
2022-06-26 14:29:11 +0200

    update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react'
-// @ts-ignore
-import {useAnalytics} from 'use-analytics'
 
 const Layout: React.FC<{className?: string}> = ({children, className = ''}) => {
-  const {page} = useAnalytics()
-  React.useEffect(() => {
-    page()
-  }, [])
-
   return (
     <div className={`px-5 min-h-screen flex w-full ${className}`}>
       {children}
