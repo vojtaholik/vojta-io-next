03b19877a5f58e2194533d90a30738f1a7f2ac4f
Vojta Holik <vojta@egghead.io>
2022-07-06 18:31:49 +0200

    update: seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import 'styles/globals.css'
 import type {AppProps} from 'next/app'
 import {DefaultSeo} from 'next-seo'
-import SEO from '../../next-seo.config'
+import SEO from '../../config'
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
