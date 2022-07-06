3ef18b75978a65b31d302bef8197b2ffd26a45b2
Vojta Holik <vojta@egghead.io>
2022-07-06 18:26:18 +0200

    update: seo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,47 @@
+import {NextSeo} from 'next-seo'
 import * as React from 'react'
 
 type LayoutProps = {
   children?: React.ReactNode
   className?: string
+  meta?: any
+  noIndex?: boolean
 }
 
-const Layout: React.FC<LayoutProps> = ({children, className = ''}) => {
+const Layout: React.FC<LayoutProps> = ({
+  children,
+  className = '',
+  meta,
+  noIndex,
+}) => {
+  const {
+    title,
+    description,
+    titleAppendSiteName = true,
+    url,
+    type = 'website',
+    ogImage,
+    date,
+  } = meta || {}
   return (
     <div className={`px-5 min-h-screen flex w-full ${className}`}>
+      <NextSeo
+        title={title}
+        description={description}
+        titleTemplate={titleAppendSiteName ? `%s | Pro Tailwind` : undefined}
+        openGraph={{
+          title,
+          description,
+          type,
+          url,
+          images: ogImage ? [ogImage] : undefined,
+          article: {
+            publishedTime: date,
+          },
+        }}
+        canonical={url}
+        noindex={noIndex}
+      />
       {children}
     </div>
   )
