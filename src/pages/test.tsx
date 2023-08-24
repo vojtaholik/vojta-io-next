831c5a898100530816847f902f85e58f1f7d6682
Vojta Holik <vojta@egghead.io>
2023-08-24 15:27:09 +0200

    test iframe embeds

---
+++ src/pages/test.tsx
@@ -0,0 +1,23 @@
+const TestPage = () => {
+  return (
+    <div className="flex flex-col space-y-5">
+      <iframe
+        src="https://epicweb.dev/tutorials/deploy-web-applications/project-setup-and-first-deployment/signing-up-for-fly-io/embed"
+        className="aspect-video"
+        sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-top-navigation"
+      />
+      <iframe
+        src="https://www.epicweb.dev/tutorials/deploy-web-applications/project-setup-and-first-deployment/intro-to-deploy-distributed-apps-with-node-js-and-fly/embed"
+        className="aspect-video"
+        sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-top-navigation"
+      />
+      <iframe
+        src="https://www.epicweb.dev/tutorials/deploy-web-applications/persisting-data-and-automatic-deployment/configure-github-actions-for-automatic-deployment/embed"
+        className="aspect-video"
+        sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-top-navigation"
+      />
+    </div>
+  )
+}
+
+export default TestPage
