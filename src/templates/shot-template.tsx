04d932227de0146472f13a0a3e0714132defc792
Vojta Holik <vojta@egghead.io>
2023-09-22 16:09:11 -0700

    shots: video seo

--- src/templates/shot-template.tsx
+++ src/templates/shot-template.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from 'components/layout'
 import Image from 'next/image'
 import {CloudinaryAsset} from 'types'
+import Head from 'next/head'
 
 type TemplateProps = {
   shot: CloudinaryAsset
@@ -24,14 +25,21 @@ const ShotTemplate: React.FC<React.PropsWithChildren<TemplateProps>> = ({
     >
       <main className="flex items-center justify-center flex-col w-full h-screen p-16">
         {shot.resource_type === 'video' ? (
-          <video
-            src={shot.secure_url}
-            controls
-            className={`aspect-[${calculateAspectRatio(
-              shot.width,
-              shot.height,
-            )}] h-full w-full`}
-          />
+          <>
+            <video
+              src={shot.secure_url}
+              controls
+              className={`aspect-[${calculateAspectRatio(
+                shot.width,
+                shot.height,
+              )}] h-full w-full`}
+            />
+            <Head>
+              <meta property="og:video" content={shot.secure_url} />
+              <meta property="og:type" content="video.movie" />
+              <meta property="og:video:type" content="video/mp4" />
+            </Head>
+          </>
         ) : (
           <Image
             className={`aspect-[${calculateAspectRatio(
