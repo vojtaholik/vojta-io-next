f500fa68352ff439f8c6f127b0a2577423159b29
Vojta Holik <vojta@egghead.io>
2023-10-12 12:18:47 +0200

    chore: clean up

--- src/utils/color-for-tag.ts
+++
@@ -1,8 +0,0 @@
-export default function colorForTag(tag: string) {
-  switch (tag) {
-    case 'starred':
-      return 'bg-amber-400'
-    default:
-      return 'bg-gray-500'
-  }
-}
--- src/utils/mailjet.ts
+++
@@ -1,7 +0,0 @@
-import {Client, SendEmailV3_1, LibraryResponse} from 'node-mailjet'
-
-export const mailjet = new Client({
-  //   apiToken: process.env.MJ_APIKEY_PUBLIC,
-  apiKey: process.env.MJ_APIKEY_PUBLIC,
-  apiSecret: process.env.MJ_APIKEY_PRIVATE,
-})
